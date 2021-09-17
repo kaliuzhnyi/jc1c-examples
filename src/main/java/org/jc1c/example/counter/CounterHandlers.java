5b70c390e5b2d12fe1b4554a5a80a5a9018a1abc
Ivan Kaliuzhnyi <kalyuzhny.ivan@gmail.com>
2021-09-18 00:28:01 +0300

    Init

--- src/main/java/org/jc1c/example/counter/CounterHandlers.java
+++ src/main/java/org/jc1c/example/counter/CounterHandlers.java
@@ -1,4 +1,34 @@
 package org.jc1c.example.counter;
 
+import org.jc1c.annotations.JHandler;
+import org.jc1c.annotations.JHandlerControllers;
+
+@JHandlerControllers
 public class CounterHandlers {
+
+    @JHandler(methodName = "plus")
+    public void methodPlusD(Double value) {
+        Counter.getInstance().plusIndex((int) value.doubleValue());
+    }
+
+    @JHandler(methodName = "plus")
+    public void methodPlusL(Long value) {
+        Counter.getInstance().plusIndex(value.intValue());
+    }
+
+    @JHandler(methodName = "minus")
+    public void methodMinusD(Double value) {
+        Counter.getInstance().minusIndex((int) value.doubleValue());
+    }
+
+    @JHandler(methodName = "minus")
+    public void methodMinusL(Long value) {
+        Counter.getInstance().minusIndex(value.intValue());
+    }
+
+    @JHandler(methodName = "get")
+    public Integer methodMinus() {
+        return Counter.getInstance().getIndex();
+    }
+
 }
