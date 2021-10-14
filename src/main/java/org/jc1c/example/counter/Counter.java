7de321013712a8f8581f1dc3c42f181b014f4a42
Ivan Kaliuzhnyi <kalyuzhny.ivan@gmail.com>
2021-10-14 19:40:32 +0300

    Update pom.xml for maven packaging.

--- src/main/java/org/jc1c/example/counter/Counter.java
+++ src/main/java/org/jc1c/example/counter/Counter.java
@@ -36,22 +36,8 @@ public class Counter {
 
     public static void main(String[] args) throws IOException {
 
-        String hostname = (args.length >= 1 && !args[0].isBlank())
-                ? args[0]
-                : null;
-
-        Integer port = (args.length >= 2 && !args[1].isBlank())
-                ? Integer.valueOf(args[1])
-                : null;
-
-        String apiKey = (args.length >= 3 && !args[2].isBlank())
-                ? args[2]
-                : null;
-
         JServer.builder()
-                .withHostname(hostname)
-                .withPort(port)
-                .withApiKey(apiKey)
+                .withArgs(args)
                 .withBacklog(3)
                 .withThreadPool(3)
                 .withHandlersController(CounterHandlers.class)
